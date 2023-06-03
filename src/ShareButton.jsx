2a257b32162e060acae95d5734e47541797407ca
Your Name <you@example.com>
2023-06-03 13:28:55 +0530

    update share button

--- src/ShareButton.jsx
+++ src/ShareButton.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import {FaShareSquare} from 'react-icons/fa';
 
 function ShareButton({url}) {
-  const handleShare = async () => {
-    try {
-      await navigator.share({
-        title: 'Share GIF',
-        url: url,
+  const handleShare = () => {
+    const imageUrl = url;
+
+    fetch(imageUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const file = new File([blob], 'my-gif.gif', { type: 'image/gif' });
+        const filesArray = [file];
+
+        if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+          navigator.share({
+            files: filesArray,
+          })
+            .then(() => console.log('Shared successfully'))
+            .catch((error) => console.error('Error sharing:', error));
+        } else {
+          console.error('Sharing files is not supported');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching GIF:', error);
       });
-      console.log('Shared successfully');
-    } catch (error) {
-      console.error('Error sharing:', error);
-    }
   };
 
-  return (
-    <button className='absolute bottom-0 right-0' onClick={handleShare}> <FaShareSquare size={32} color="#4FCE5D" /> </button>
-  );
+  return <button className='absolute bottom-0 right-0' onClick={handleShare}> <FaShareSquare size={32} color="#4FCE5D" /> </button>
 }
 
 export default ShareButton;
